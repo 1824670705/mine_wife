8ccac5fcf6c6591f3a1c4ec8a13b51371665de18
L <1824670705@qq.com>
2022-07-30 10:02:51 +0800

    【报表数据】 -- 完善报表数据

--- src/main/java/com/oa/application/order/service/impl/OaOrderReportServiceImpl.java
+++ src/main/java/com/oa/application/order/service/impl/OaOrderReportServiceImpl.java
@@ -5,6 +5,7 @@ import com.oa.application.order.entity.dto.OaOrderReportDto;
 import com.oa.application.order.entity.vo.OaOrderReportVo;
 import com.oa.application.order.service.OaOrderReportService;
 import com.oa.domain.mapper.OaOrderMapper;
+import com.oa.utils.other.LoginUserInfoUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -17,6 +18,7 @@ public class OaOrderReportServiceImpl implements OaOrderReportService {
 
     @Override
     public Page<OaOrderReportVo> query(OaOrderReportDto oaOrderReportDto) {
+        oaOrderReportDto.setCreateBy(LoginUserInfoUtils.getLoginUserId());
         return oaOrderMapper.query(new Page<>(oaOrderReportDto.getPageIndex(), oaOrderReportDto.getPageSize()), oaOrderReportDto);
     }
 }
